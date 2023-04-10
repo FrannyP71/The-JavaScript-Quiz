f646f862b92190925f7fef8406c60a0f5e222ed1
FrannyP71 <127154382+FrannyP71@users.noreply.github.com>
2023-04-10 13:16:24 -0400

    finished question array

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ var questionArr=[
         answer: "JavaScript",
     },
 
-    {   question: "What does JS stand for?",
-        responses: ["Jersey Shore", "John Sinclair", "John Sheriden", "JavaScript"], 
-        answer: "JavaScript",
+    {   question: "Arrays in Javascript can be used to store?",
+        responses: ["booleans", "strings", "numbers", "All of the above"], 
+        answer: "All of the above",
     },
 
-    {   question: "What does JS stand for?",
-        responses: ["Jersey Shore", "John Sinclair", "John Sheriden", "JavaScript"], 
-        answer: "JavaScript",
+    {   question: "Commonly used data types do not include?",
+        responses: ["alerts", "booleans", "numbers", "strings"], 
+        answer: "alerts",
     },
 
-    {   question: "What does JS stand for?",
-        responses: ["Jersey Shore", "John Sinclair", "John Sheriden", "JavaScript"], 
-        answer: "JavaScript",
+    {   question: "What's an example of how to write a variable name?",
+        responses: ["21jumpStreet", "jump Street", "jumpStreet", "$21JumpStreet"], 
+        answer: "jumpStreet",
     },
-    
-    {   question: "What does JS stand for?",
-        responses: ["Jersey Shore", "John Sinclair", "John Sheriden", "JavaScript"], 
-        answer: "JavaScript",
+
+    {   question: "Strings must be enclosed with ___?",
+        responses: ["quotation marks", "parenthesis", "curly brackets", "commas"], 
+        answer: "quotation marks",
     },
     //copy and paste questions
 ]
