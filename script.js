6a9e605ccc049b9e5324a95b7209efc57d9cd57d
FrannyP71 <127154382+FrannyP71@users.noreply.github.com>
2023-04-10 12:58:11 -0400

    started on the JS file

---
+++ script.js
@@ -0,0 +1,36 @@
+var startButton=document.getElementById("start-button")
+var timeLeft=60
+var questionArr=[
+    {   question: "What does JS stand for?",
+        responses: ["Jersey Shore", "John Sinclair", "John Sheriden", "JavaScript"], 
+        answer: "JavaScript",
+    },
+
+    {   question: "What does JS stand for?",
+        responses: ["Jersey Shore", "John Sinclair", "John Sheriden", "JavaScript"], 
+        answer: "JavaScript",
+    },
+
+    {   question: "What does JS stand for?",
+        responses: ["Jersey Shore", "John Sinclair", "John Sheriden", "JavaScript"], 
+        answer: "JavaScript",
+    },
+
+    {   question: "What does JS stand for?",
+        responses: ["Jersey Shore", "John Sinclair", "John Sheriden", "JavaScript"], 
+        answer: "JavaScript",
+    },
+    
+    {   question: "What does JS stand for?",
+        responses: ["Jersey Shore", "John Sinclair", "John Sheriden", "JavaScript"], 
+        answer: "JavaScript",
+    },
+    //copy and paste questions
+]
+
+
+
+
+startButton.addEventListener("click", function(event) {
+    console.log(event)
+})
